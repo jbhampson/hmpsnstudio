692ff66dec1e7b38b5a8ce824e9a7df77740828a
jbhampson <30542485+jbhampson@users.noreply.github.com>
2023-03-10 00:28:30 -0600

    save

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ barba.init({
       async leave(data) {
         console.log('leave');
         console.log(data);
-        await gsap.to(data.current.container, { opacity: 0, duration: 1 });
+        await gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
       },
       async enter(data) {
         console.log('enter');
         console.log(data);
-        await gsap.to(data.next.container, { opacity: 1, duration: 1 });
+        await gsap.to(data.next.container, { opacity: 1, duration: 0.5 });
       },
     },
   ],
