7ef972057d8e58b64beff253140ff525ef63f59e
jbhampson <30542485+jbhampson@users.noreply.github.com>
2023-03-10 08:57:58 -0600

    SAVE

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,15 @@ barba.init({
         console.log(data);
         await gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
       },
-      async enter(data) {
+      async afterEnter(data) {
         console.log('enter');
         console.log(data);
         await gsap.to(data.next.container, { opacity: 1, duration: 0.5 });
       },
     },
   ],
+});
+
+barba.hooks.after(async () => {
+  await restartWebflow();
 });
